13d3bf6bbd5b7d51968b0ddc1e8c3d37cc07e9b6
Carles Aguilar <Carles.Aguilar1@ibm.com>
2021-11-23 12:26:25 +0100

    add dependencies

--- app/src/main/java/com/carlesaguilar/todolist/MainActivity.kt
+++ app/src/main/java/com/carlesaguilar/todolist/MainActivity.kt
@@ -3,11 +3,6 @@ package com.carlesaguilar.todolist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.carlesaguilar.todolist.ui.theme.ToDoListTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +10,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ToDoListTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ToDoListTheme {
-        Greeting("Android")
     }
 }
