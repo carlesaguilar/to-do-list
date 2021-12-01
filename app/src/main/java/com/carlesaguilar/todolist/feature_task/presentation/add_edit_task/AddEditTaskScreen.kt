2b4cd7e1ed88211da053e08b87f4a883b6ed193f
Carles Aguilar <Carles.Aguilar1@ibm.com>
2021-12-01 17:36:23 +0100

    create add/edit task screen

---
+++ app/src/main/java/com/carlesaguilar/todolist/feature_task/presentation/add_edit_task/AddEditTaskScreen.kt
@@ -0,0 +1,133 @@
+package com.carlesaguilar.todolist.feature_task.presentation.add_edit_task
+
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Save
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.carlesaguilar.todolist.feature_task.domain.model.Task
+import com.carlesaguilar.todolist.feature_task.presentation.add_edit_task.components.TransparentHintTextField
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+@Composable
+fun AddEditTaskScreen(
+    navController: NavController,
+    taskColor: Int,
+    viewModel: AddEditTaskViewModel = hiltViewModel()
+) {
+    val titleState = viewModel.taskTitle.value
+    var scaffoldState = rememberScaffoldState()
+    val taskBackgroundAnimatable = remember {
+        Animatable(
+            Color(if (taskColor != -1) taskColor else viewModel.taskColor.value)
+        )
+    }
+    val scope = rememberCoroutineScope()
+
+    LaunchedEffect(key1 = true) {
+        viewModel.eventFlow.collectLatest { event ->
+            when (event) {
+                is AddEditTaskViewModel.UiEvent.SaveTask -> {
+                    navController.navigateUp()
+                }
+                is AddEditTaskViewModel.UiEvent.ShowSnackbar -> {
+                    scaffoldState.snackbarHostState.showSnackbar(
+                        message = event.message
+                    )
+                }
+            }
+        }
+    }
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    viewModel.onEvent(AddEditTaskEvent.SaveTask)
+                },
+                backgroundColor = MaterialTheme.colors.primary
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Save,
+                    contentDescription = "Save task"
+                )
+            }
+            scaffoldState = scaffoldState
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(taskBackgroundAnimatable.value)
+                .padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Task.taskColors.forEach { color ->
+                    val colorInt = color.toArgb()
+                    Box(
+                        modifier = Modifier
+                            .size(50.dp)
+                            .shadow(15.dp, CircleShape)
+                            .clip(CircleShape)
+                            .background(color)
+                            .border(
+                                width = 3.dp,
+                                color = if (viewModel.taskColor.value == colorInt) {
+                                    Color.Black
+                                } else {
+                                    Color.Transparent
+                                }
+                            )
+                            .clickable {
+                                scope.launch {
+                                    taskBackgroundAnimatable.animateTo(
+                                        targetValue = Color(colorInt),
+                                        animationSpec = tween(
+                                            durationMillis = 500
+                                        )
+                                    )
+                                }
+                                viewModel.onEvent(AddEditTaskEvent.ChangeColor(colorInt))
+                            }
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            TransparentHintTextField(
+                text = titleState.text,
+                hint = titleState.hint,
+                onValueChange = {
+                    viewModel.onEvent(AddEditTaskEvent.EnteredTitle(it))
+                },
+                onFocusChange = {
+                    viewModel.onEvent(AddEditTaskEvent.ChangeTitleFocus(it))
+                },
+                isHintVisible = titleState.isHintVisible,
+                textStyle = MaterialTheme.typography.h5
+            )
+        }
+    }
+}
